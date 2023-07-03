33cc18d9144a7b4e73227f25810ec66c138b1870
shalo <shalo@ShalooBangaLen>
2023-07-03 08:48:38 -0400

    Adding comment in LocalDateProvider.java

--- movie-theater/src/main/java/com/jpmc/theater/util/LocalDateProvider.java
+++ movie-theater/src/main/java/com/jpmc/theater/util/LocalDateProvider.java
@@ -2,9 +2,10 @@ package com.jpmc.theater.util;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 /*
- * Singleton to provide date/time related methods
+ * Java Singleton to provide date/time related methods
  * It is created as Enum to no need to worry about creating new instance during serialization/deserialization or using reflection 
- */
+ * As this application is using Spring, and spring beans are singleton in one application context, we could create it as Spring bean and let Spring manage the life cycle. 
+  */
 public enum LocalDateProvider {
 	INSTANCE;
 
